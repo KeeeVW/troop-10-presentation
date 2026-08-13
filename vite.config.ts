7a9b2ba680b7490d4d03fefa9209a9fbf8be79f3
KeeeVW <kevinwael32@gmail.com>
2026-08-14 02:56:06 +0300

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/plan/" : "/",
+export default defineConfig({
+  base: '/plan/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-}));
+})
